d37873facd4646199620307d3ad810830c0fdfe2
Txr Lucas <lucas.cult@gmail.com>
2023-10-25 15:04:45 -0300

    Implementamos o hello world multilinguas

--- hello.py
+++ hello.py
@@ -1,3 +1,35 @@
-print("Hello, World!")
-print('lucas'.upper())
-print(56 + 7)
+"""Hello World Multi Linguas
+
+Dependendo da língua configurada no ambiente o programa exibe a mensagem
+correspondente.
+
+Como usar:
+
+Tenha a variável LANG devidamente configurada ex:
+
+	export LANG=pt_BR
+
+Execução:
+	
+	python3 hello.py
+	ou
+	./hello.py
+"""
+__version__= "0.01"
+__author__ = "Lucas Teixeira"
+__license__ = "Unlicense"
+
+import	os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+	msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+	msg = "Ciao, Mondo!"
+
+
+print(msg)
+
